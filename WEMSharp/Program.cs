b367885291e861117fc1bbc24374b07b5dba6204
Andrew Sun <me@andrewsun.com>
2019-03-18 21:53:06 -0400

    I have no idea what I'm doing but it compiles

---
+++ WEMSharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WEMSharp;
+
+namespace WEMSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Usage: wemsharp.exe <helper file> <input file> <output file>");
+                return;
+            }
+            WEMFile wem = new WEMFile(args[1], WEMForcePacketFormat.NoForcePacketFormat);
+            wem.GenerateOGG(args[2], args[0], false, false);
+        }
+    }
+}
